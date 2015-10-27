dc79e2564112f3918bf429ea3651995dffb4e9cf
fabMar2016 <fabio9608@live.it>
2015-10-27 09:07:10 +0100

    Update sequenza.c

--- sequenza.c
+++ sequenza.c
@@ -12,3 +12,29 @@ typedef struct s_nodo nodo;
    int    verso: crescente (0) o decrescente (1)
 */
 void ordina(nodo** seq, int verso);
+//inizio Marini Basso
+void bubbleSort(nodo** sequence,int len){
+    for(int i =0 ; i< len -1; i++){
+        nodo *tmp=*sequence;
+        nodo* prev= tmp;
+        for(int j=0;j<len-1;j++){
+            prev=tmp;
+            tmp=tmp->next;
+            if(prev->info < tmp->info){
+                prev=tmp->next;
+                tmp=prev;
+            }
+        }
+    }
+}
+
+int lengthSequence(nodo** sequence){
+    int nNode=0;
+    nodo* tmp=*sequence;
+    while(tmp!=NULL){
+        nNode++;
+        tmp=tmp->next;
+    }
+    return nNode;
+}
+//fine Marini Basso
