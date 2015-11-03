3d9bbf637b8f952d0bc3e69ef04e31bfee330599
nicobenna <nikobenna@gmail.com>
2015-11-03 20:46:30 +0100

    Implementata pila statica
    svolto da Benacchio,Carpanedo,Kerber,Sguario,Zotta

--- pila_statica.c
+++ pila_statica.c
@@ -1,1 +1,33 @@
-// init
+//pila statica Benacchio,Carpanedo,Kerber,Sguario,Zotta
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define max_stack 1000
+
+struct stack{
+  int array[max_stack];
+  int indice = 0;         //indice di inserimento
+}
+
+void push(stack pila, int a)
+{
+  if(pila.indice == max_stack)
+    printf("Stack overflow");
+  else
+  {
+    pila.array[pila.indice] = a;
+    pila.indice++;
+  }
+}
+
+int pop(stack pila)
+{
+  if(pila.indice == max_stack)
+    printf("Stack overflow");
+  else
+  {
+    pila.indice--;
+    return pila.array[pila.indice]
+  }
+}
