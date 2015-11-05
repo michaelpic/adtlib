0933a1d1ac59bd451479b9af26a3a718371df946
michaelpic <miticosud@gmail.com>
2015-11-05 19:29:58 +0100

    Implementato coda statica
    Fatto da Michael Piccinni

--- coda_statica.c
+++ coda_statica.c
@@ -1,1 +1,21 @@
-// init
+void enqueue(int lunghezza, int ultimate,int V[],int val)
+{
+  if(lunghezza>ultimate)
+  {
+    V[ultimate-1] = val;
+    ultimate++;
+  }
+  else
+    printf("Coda piena\n");
+}
+
+int dequeue(int lughezza, int ultimate, int v[])
+{
+  int tmp = v[0];
+  int i;
+  for(i=0;i<ultimate-1;i++)
+  {
+    v[i]=v[i+1];
+  }
+  return tmp;
+}
